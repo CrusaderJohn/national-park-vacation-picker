b3721cec5f0e0dac93b05c3c3a35ad565d327fba
John Jamieson <jamieson.john@gmail.com>
2022-11-23 18:46:16 -0500

    Update to output warm week

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,23 +89,42 @@ function displayLinks() {
   }
 }
 
-function weatherAPI()
+function weather()
 {
+    console.log("test");
+
     let weatherURL = "https://api.weather.gc.ca/collections/climate-daily/items";
     let formatURL = "?f=json";
-    let year = "&LOCAL_YEAR=2020";
-    let station = "&CLIMATE_IDENTIFIER=605DJ25";
+    let year = "&LOCAL_YEAR=2021";
+    let station = "&CLIMATE_IDENTIFIER=6115811";
     let finalURL = "";
+    let hottestDay = -200;
+    let weekSum = 0;
+    let weekAvg = 0;
 
-    if (station)
-    {
-        finalURL = weatherURL + formatURL + year + station;
+    class Day {
+        constructor(year, month, day, temp, rain, longDay) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.temp = temp;
+            this.rain = rain;
+            this.longDay = longDay;
+        }
     }
 
-    console.log(finalURL);
+    class Year {
+        constructor() {
+            this.days = [];
+            this.hotWeekTemp = -200;
+            this.hotWeekDay = 0;
+            this.dryWeek = -200;
+            this.dryWeekDay = 0;
+        }
+    }
 
-    // 6115811.2021.2.12
-    // https://api.weather.gc.ca/collections/climate-stations/items?limit=100&startindex=0&COUNTRY=CAN&DLY_LAST_DATE=2022-11-14+00%3A00%3A00&ENG_PROV_NAME=ontario
+    finalURL = weatherURL + formatURL + year + station;
+    let newYear = new Year(2021);
 
     fetch(finalURL)
         .then(function (response)
@@ -118,31 +137,52 @@ function weatherAPI()
         })
         .then(function (jsonResult)
         {
-            console.log(jsonResult.features.length);
-            console.log(`Temp: ${jsonResult.features[jsonResult.features.length - 1].properties.MEAN_TEMPERATURE}`);
-            console.log(`Rain: ${jsonResult.features[jsonResult.features.length - 1].properties.TOTAL_PRECIPITATION}`);
-            console.log(jsonResult);
-        })
-        .catch(function (error)
-        {
-            console.error(error);
-        });
-
-    fetch("https://api.weather.gc.ca/collections/climate-daily/items?f=json&ID=6115811.2021.2.12")
-        .then(function (response)
-        {
-            if (!response.ok)
-            {
-                throw response.json();
+            for (let i = 0; i < jsonResult.features.length; i++) {
+                // console.log(newYear);
+                newYear.days.push(new Day(
+                    jsonResult.features[i].properties.LOCAL_YEAR,
+                    jsonResult.features[i].properties.LOCAL_MONTH,
+                    jsonResult.features[i].properties.LOCAL_DAY,
+                    jsonResult.features[i].properties.MEAN_TEMPERATURE,
+                    jsonResult.features[i].properties.TOTAL_PRECIPITATION,
+                    jsonResult.features[i].properties.LOCAL_DATE));
             }
-            return response.json();
-        })
-        .then(function (jsonResult)
-        {
-            console.log(jsonResult.features.length);
-            console.log(`Temp: ${jsonResult.features[jsonResult.features.length - 1].properties.MEAN_TEMPERATURE}`);
-            console.log(`Rain: ${jsonResult.features[jsonResult.features.length - 1].properties.TOTAL_PRECIPITATION}`);
-            console.log(jsonResult);
+
+            // The below lines sort the array of days into chronological order.
+            // Based off: https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+            newYear.days.sort((a, b) => {
+                let da = new Date(a.longDay);
+                let db = new Date(b.longDay);
+                return da - db;
+            });
+
+            for (let i = 0; i < jsonResult.features.length - 7; i++)
+            {
+                weekSum = newYear.days[i].temp + newYear.days[i+1].temp + newYear.days[i+2].temp +
+                    newYear.days[i+3].temp + newYear.days[i+4].temp + newYear.days[i+5].temp + newYear.days[i+6].temp;
+                weekAvg = weekSum / 7;
+                if (weekAvg > newYear.hotWeekTemp)
+                {
+                    newYear.hotWeekTemp = weekAvg;
+                    newYear.hotWeekDay = newYear.days[i].longDay;
+                }
+            }
+
+            for (let i = 0; i < jsonResult.features.length - 7; i++)
+            {
+                weekSum = newYear.days[i].rain + newYear.days[i+1].rain + newYear.days[i+2].rain +
+                    newYear.days[i+3].rain + newYear.days[i+4].rain + newYear.days[i+5].rain + newYear.days[i+6].rain;
+                weekAvg = weekSum / 7;
+                if (weekAvg > newYear.dryWeek)
+                {
+                    newYear.dryWeek = weekAvg;
+                    newYear.dryWeekDay = newYear.days[i].longDay;
+                }
+            }
+
+            console.log(newYear.hotWeekDay);
+            console.log(newYear.dryWeekDay);
+
         })
         .catch(function (error)
         {
@@ -150,4 +190,4 @@ function weatherAPI()
         });
 }
 
-weatherAPI();
+weather();
